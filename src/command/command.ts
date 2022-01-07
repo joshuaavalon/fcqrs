871ba1b8eb5368bea6d689f307d890147bff68dc
Joshua Avalon <git@avalon.sh>
2022-01-07 17:22:54 +0800

    fix: limit command & event to readonly

--- src/command/command.ts
+++ src/command/command.ts
@@ -2,6 +2,6 @@ import type { CommandType } from "./command-type";
 import type { Commands } from "./commands";
 
 export interface Command<T extends CommandType = CommandType> {
-  type: T;
-  payload: Commands[T];
+  readonly type: T;
+  readonly payload: Commands[T];
 }
--- src/event/event.ts
+++ src/event/event.ts
@@ -2,6 +2,6 @@ import type { EventType } from "./event-type";
 import type { Events } from "./events";
 
 export interface Event<T extends EventType = EventType> {
-  type: T;
-  payload: Events[T];
+  readonly type: T;
+  readonly payload: Events[T];
 }
